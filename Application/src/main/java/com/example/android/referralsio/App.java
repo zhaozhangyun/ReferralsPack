ab0ebf8428bb9ed170c782d7ac84cffcdca5408c
zhangyun.zhao <zhaozhangyun@outlook.com>
2019-06-13 18:55:23 -0800

    add xgpush

--- Application/src/main/java/com/example/android/referralsio/App.java
+++ Application/src/main/java/com/example/android/referralsio/App.java
@@ -1,13 +1,17 @@
 package com.example.android.referralsio;
 
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 
+import com.tencent.android.tpush.XGPushManager;
 import com.tencent.stat.MtaSDkException;
 import com.tencent.stat.StatConfig;
 import com.tencent.stat.StatService;
 import com.tencent.stat.common.StatConstants;
+
+import java.util.List;
 
 import io.referrals.lib.ReferralsHolder;
 import io.referrals.lib.configuration.ReferralsConfiguration;
@@ -25,6 +29,24 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
+        // 在主进程设置信鸽相关的内容
+        if (isMainProcess()) {
+            // 为保证弹出通知前一定调用本方法，需要在application的onCreate注册
+            // 收到通知时，会调用本回调函数。
+            // 相当于这个回调会拦截在信鸽的弹出通知之前被截取
+            // 一般上针对需要获取通知内容、标题，设置通知点击的跳转逻辑等等
+            XGPushManager.setNotifactionCallback(xGNotifaction -> {
+                Log.i(TAG, "处理信鸽通知：" + xGNotifaction);
+                // 获取标签、内容、自定义内容
+                String title = xGNotifaction.getTitle();
+                String content = xGNotifaction.getContent();
+                String customContent = xGNotifaction.getCustomContent();
+                // 其它的处理
+                // 如果还要弹出通知，可直接调用以下代码或自己创建Notifaction，否则，本通知将不会弹出在通知栏中。
+                xGNotifaction.doNotify();
+            });
+        }
+
         try {
             // 打开Logcat输出，上线时，一定要关闭
             StatConfig.setDebugEnable(BuildConfig.DEBUG);
@@ -41,5 +63,18 @@ public class App extends Application {
                 })
                 .build();
         ReferralsHolder.fire(this, configuration);
+    }
+
+    private boolean isMainProcess() {
+        ActivityManager am = ((ActivityManager) getSystemService(Context.ACTIVITY_SERVICE));
+        List<ActivityManager.RunningAppProcessInfo> processInfos = am.getRunningAppProcesses();
+        String mainProcessName = getPackageName();
+        int myPid = android.os.Process.myPid();
+        for (ActivityManager.RunningAppProcessInfo info : processInfos) {
+            if (info.pid == myPid && mainProcessName.equals(info.processName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Application/src/main/java/com/example/android/referralsio/EntryListActivity.java
+++ Application/src/main/java/com/example/android/referralsio/EntryListActivity.java
@@ -2,6 +2,11 @@ package com.example.android.referralsio;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
 
 /**
  * Activity for holding EntryListFragment.
@@ -12,5 +17,26 @@ public class EntryListActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_list);
+
+        XGPushConfig.enableDebug(this, BuildConfig.DEBUG);
+        XGPushConfig.enableOtherPush(getApplicationContext(), true);
+//        XGPushConfig.setHuaweiDebug(true);
+        XGPushConfig.setMiPushAppId(getApplicationContext(), "2882303761518026703");
+        XGPushConfig.setMiPushAppKey(getApplicationContext(), "5351802651703");
+//        XGPushConfig.setMzPushAppId(this, "MZ_APPID");
+//        XGPushConfig.setMzPushAppKey(this, "MZ_APPKEY");
+
+        XGPushManager.registerPush(this, new XGIOperateCallback() {
+            @Override
+            public void onSuccess(Object data, int flag) {
+                //token在设备卸载重装的时候有可能会变
+                Log.d("TPush", "注册成功，设备token为：" + data);
+            }
+
+            @Override
+            public void onFail(Object data, int errCode, String msg) {
+                Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
+            }
+        });
     }
 }
